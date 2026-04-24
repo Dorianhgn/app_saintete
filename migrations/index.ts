74db48b075f02d0e158d242097eef26d78c903a8
Dorianhgn <dorianhugonnet1@gmail.com>
2026-04-24 12:22:25 +0200

    feat: add migration index and update package scripts
    - Created a new migration index file to manage migrations.
    - Updated package.json to include new scripts for payload operations and removed unnecessary build step.
    - Regenerated payload-types.ts to reflect updated collection and interface definitions.
    - Adjusted imports in payload.config.ts to include file extensions for TypeScript compatibility.
    - Modified tsconfig.json to allow importing TypeScript file extensions.

---
+++ migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260424_102051 from './20260424_102051';
+
+export const migrations = [
+  {
+    up: migration_20260424_102051.up,
+    down: migration_20260424_102051.down,
+    name: '20260424_102051'
+  },
+];
